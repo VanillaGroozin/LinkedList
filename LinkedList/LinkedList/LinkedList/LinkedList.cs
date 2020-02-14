b3af1466e7c1677cb94a2519df20aeda4def3674
Eldar Gugunishvili <vanilla38penguin@outlook.com>
2020-02-15 00:02:12 +0600

    Реализвация двусвязного списка

---
+++ LinkedList/LinkedList/LinkedList/LinkedList.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList.LinkedList
+{
+    public class LinkedList<T> : IEnumerable<T>
+    {
+        Node<T> head; // головной элемент
+        Node<T> tail; // последний элемент
+        int count;  // количество элементов в списке
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)this).GetEnumerator();
+        }
+
+        // Метод для вставки новых элементов в конец списка.
+        public void Add(T data)
+        {
+            Node<T> node = new Node<T>(data);
+
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                tail.Next = node;
+                node.Previous = tail;
+            }
+
+            tail = node;
+
+            count++;
+        }
+
+        // Метод для вставки новых элементов в произвольную позицию, по индексу.
+        public void Add(T data, int index)
+        {
+            if (index < 0 || index > count)
+                throw new System.IndexOutOfRangeException();
+
+            Node<T> node = new Node<T>(data);
+
+            if (index != 0)
+            {
+                var current = GetNode(index);
+
+                node.Previous = current.Previous;
+                node.Next = current;
+                current.Previous.Next = node;
+                current.Previous = node;
+            }
+            else
+            {
+                node.Next = head;
+                head = node;
+            }
+
+            count++;
+        }
+
+        // Метод для удаления элементов списка по индексу.
+        public void Remove(int index)
+        {
+            if (index < 0 || index >= count)
+                throw new System.IndexOutOfRangeException();
+
+            if (index != 0)
+            {
+                var current = GetNode(index);
+                current.Previous.Next = current.Next.Previous;
+                current.Next.Previous = current.Previous;
+                current.Previous.Next = current.Next;
+            }
+            count--;
+        }
+
+        // Метод для получения элемента списка по индексу.
+        public T Get(int index)
+        {
+            if (index < 0 || index >= count)
+                throw new System.IndexOutOfRangeException();
+
+            int counter = 0;
+            Node<T> current = head;
+
+            while (current != null && counter != index)
+            {
+                counter++;
+                current = current.Next;
+            }
+            return current.Data;
+        }
+
+        private Node<T> GetNode(int index)
+        {
+            if (index < 0 || index >= count)
+                throw new System.IndexOutOfRangeException();
+
+            int counter = 0;
+            Node<T> current = head;
+
+            while (current != null && counter != index)
+            {
+                counter++;
+                current = current.Next;
+            }
+            return current;
+        }
+
+        // Свойство или метод для получения количества элементов в списке
+        public int Count { get { return count; } }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            Node<T> current = head;
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
+
+        #region QSort
+        private static void Swap(Node<T> x, Node<T> y)
+        {
+            var t = x.Data;
+            x.Data = y.Data;
+            y.Data = t;
+        }
+
+        // метод возвращающий индекс опорного элемента
+        static int Partition(LinkedList<T> list, int minIndex, int maxIndex)
+        {
+            Type typeParameterType = typeof(T);
+            var pivot = minIndex - 1;
+            for (var i = minIndex; i < maxIndex; i++)
+            {
+                switch (Type.GetTypeCode(typeParameterType))
+                {
+                    case TypeCode.String:
+                        if (string.Compare(list.GetNode(i).Data.ToString(),
+                        list.GetNode(maxIndex).Data.ToString()) == -1)
+                        {
+                            pivot++;
+                            Swap(list.GetNode(pivot), list.GetNode(i));
+                        }
+                        break;
+                    case TypeCode.Int32:
+                        if (int.Parse(list.GetNode(i).Data.ToString()) <
+                            int.Parse(list.GetNode(maxIndex).Data.ToString()))
+                        {
+                            pivot++;
+                            Swap(list.GetNode(pivot), list.GetNode(i));
+                        }
+                        break;
+                    case TypeCode.Double:
+                        if (double.Parse(list.GetNode(i).Data.ToString()) <
+                            double.Parse(list.GetNode(maxIndex).Data.ToString()))
+                        {
+                            pivot++;
+                            Swap(list.GetNode(pivot), list.GetNode(i));
+                        }
+                        break;
+                    case TypeCode.Decimal:
+                        if (decimal.Parse(list.GetNode(i).Data.ToString()) <
+                            decimal.Parse(list.GetNode(maxIndex).Data.ToString()))
+                        {
+                            pivot++;
+                            Swap(list.GetNode(pivot), list.GetNode(i));
+                        }
+                        break;
+                    case TypeCode.DateTime:
+                        if (DateTime.Parse(list.GetNode(i).Data.ToString()) <
+                            DateTime.Parse(list.GetNode(maxIndex).Data.ToString()))
+                        {
+                            pivot++;
+                            Swap(list.GetNode(pivot), list.GetNode(i));
+                        }
+                        break;
+                    default:
+                        throw new Exception("Unsupported datatype");
+                }
+
+            }
+            pivot++;
+            Swap(list.GetNode(pivot), list.GetNode(maxIndex));
+            return pivot;
+        }
+
+        // быстрая сортировка
+        private static LinkedList<T> QuickSort(LinkedList<T> list, int minIndex, int maxIndex)
+        {
+            if (minIndex >= maxIndex)
+            {
+                return list;
+            }
+
+            var pivotIndex = Partition(list, minIndex, maxIndex);
+            QuickSort(list, minIndex, pivotIndex - 1);
+            QuickSort(list, pivotIndex + 1, maxIndex);
+
+            return list;
+        }
+
+        public LinkedList<T> QuickSort()
+        {
+            return QuickSort(this, 0, this.Count - 1);
+        }
+        #endregion
+    }
+}
--- LinkedList/LinkedList/Program.cs
+++ LinkedList/LinkedList/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LinkedList.LinkedList;
 
 namespace test
 {
@@ -11,16 +12,20 @@ namespace test
     {
         static void Main(string[] args)
         {
-            LinkedList<string> linkedString = new LinkedList<string>();
-            LinkedList<int> linkedInt = new LinkedList<int>();
+            LinkedList.LinkedList.LinkedList<string> linkedString = new LinkedList.LinkedList.LinkedList<string>();
+            LinkedList.LinkedList.LinkedList<int> linkedInt = new LinkedList.LinkedList.LinkedList<int>();
 
-            // добавление элементов
-            linkedString.Add("Alesha");
+            // добавление элементов          
+            linkedString.Add("Sasha");
             linkedString.Add("Alex");
+            linkedString.Add("Sanya");
+            linkedString.Add("Alesha");
+            linkedString.Add("Boris");
+            linkedString.Add("Mark");
+            linkedString.Add("Gosha");
+            linkedString.Add("Eldar");
             linkedString.Add("Aleksey");
             linkedString.Add("Aleksandr");
-            linkedString.Add("Sasha");
-            linkedString.Add("Sanya");
 
             // добавление элемента в определенную позицию
             linkedString.Add("Saniya", 4);
@@ -30,146 +35,63 @@ namespace test
             linkedString.Remove(4);
             Console.WriteLine($"4. элемент: {linkedString.Get(4)}"); ;
 
-            // выводим элементы
+            // выводим элементы с "Ale"
             foreach (var item in linkedString.Where(x => x.Contains("Ale")))
             {
                 Console.WriteLine(item);
             }
+            // выводим количество элементов
+            Console.WriteLine($"Количество элементов: {linkedString.Count}");
 
-            int count = linkedString.Count;
-
-
-
-            Console.ReadKey();
-        }
-    }
-
-    public class Node<T>
-    {
-        public Node(T data)
-        {
-            Data = data;
-        }
-        public T Data { get; set; }
-        public Node<T> Next { get; set; }
-        public int Index { get; set; }
-    }
-
-    public class LinkedList<T> : IEnumerable<T>
-    {
-        Node<T> head; // головной элемент
-        Node<T> tail; // последний элемент
-        int count;  // количество элементов в списке
-
-        // Метод для вставки новых элементов в конец списка.
-        public void Add(T data)
-        {
-            Node<T> node = new Node<T>(data);
-
-            if (head == null)
-                head = node;
-            else
-                tail.Next = node;
-            tail = node;
-
-            count++;
-        }
-
-        // Метод для вставки новых элементов в произвольную позицию, по индексу.
-        public void Add(T data, int index)
-        {
-            if (index < 0 || index > count)
-                throw new System.IndexOutOfRangeException();
-
-            int counter = 0;
-            Node<T> node = new Node<T>(data);
-
-            if (index != 0)
+            // выводим все элементы
+            Console.Write("Неотсортированный лист: ");
+            foreach (var item in linkedString)
             {
-                Node<T> current = head;
-                Node<T> previous = null;
-                while (current != null && counter != index)
-                {
-                    counter++;
-                    previous = current;
-                    current = current.Next;
-                }
-
-                node.Next = current;
-                previous.Next = node;
+                Console.Write(item + " ");
             }
-            else
+            Console.WriteLine();
+
+            // сортируем
+            linkedString.QuickSort();
+            // выводим все отсортированные элементы
+            Console.Write("Отсортированный лист: ");
+            foreach (var item in linkedString)
             {
-                node.Next = head;
-                head = node;
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+
+            // другие типы тоже работают
+            linkedInt.Add(1);
+            linkedInt.Add(6);
+            linkedInt.Add(8);
+            linkedInt.Add(3);
+            linkedInt.Add(10);
+            linkedInt.Add(5);
+            linkedInt.Add(2);
+            linkedInt.Add(4);
+            linkedInt.Add(7);
+            linkedInt.Add(9);
+
+            // выводим все элементы
+            Console.Write("Неотсортированный лист: ");
+            foreach (var item in linkedInt)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            // сортируем
+            linkedInt.QuickSort();
+            // выводим все отсортированные элементы
+            Console.Write("Отсортированный лист: ");
+            foreach (var item in linkedInt)
+            {
+                Console.Write(item + " ");
             }
 
-            count++;
+            Console.ReadLine();
         }
-
-        // Метод для удаления элементов списка по индексу.
-        public void Remove(int index)
-        {
-            if (index < 0 || index >= count)
-                throw new System.IndexOutOfRangeException();
-
-            int counter = 0;
-            Node<T> current = head;
-            Node<T> previous = null;
-
-            if (index != 0)
-            {
-                while (current != null && counter != index)
-                {
-                    counter++;                  
-
-                    previous = current;
-                    current = current.Next;
-                }
-
-                previous.Next = current.Next;
-            }
-            else
-            {
-                head = current.Next;
-            }
-            count--;
-        }
-
-        // Метод для получения элемента списка по индексу.
-        public T Get(int index)
-        {
-            if (index < 0 || index >= count)
-                throw new System.IndexOutOfRangeException();
-
-            int counter = 0;
-            Node<T> current = head;
-
-            while (current != null && counter != index)
-            {
-                counter++;
-                current = current.Next;
-            }
-            return current.Data;
-        }
-
-
-        // Свойство или метод для получения количества элементов в списке.
-        public int Count { get { return count; } }
-
-        IEnumerator<T> IEnumerable<T>.GetEnumerator()
-        {
-            Node<T> current = head;
-            while (current != null)
-            {
-                yield return current.Data;
-                current = current.Next;
-            }
-        }
-        public IEnumerator GetEnumerator()
-        {
-            return ((IEnumerable)this).GetEnumerator();
-        }
-
     }
 }
